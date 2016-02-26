c1b0db352f812e5027df69d66d8ba0735d82a56b
刚 张 <gangzhang@gangdeMacBook-Pro.local>
2016-02-26 20:42:26 +0800

    second commit

--- app/src/main/obj/local/armeabi/objs/live555/UsageEnvironment/HashTable.o.d
+++
@@ -1,8 +0,0 @@
-/Users/gangzhang/Documents/videotest1/app/src/main/obj/local/armeabi/objs/live555/UsageEnvironment/HashTable.o: \
- /Users/gangzhang/Documents/videotest1/app/src/main/jni/UsageEnvironment/HashTable.cpp \
- /Users/gangzhang/Documents/videotest1/app/src/main/jni/UsageEnvironment/include/HashTable.hh \
- /Users/gangzhang/Documents/videotest1/app/src/main/jni/UsageEnvironment/include/Boolean.hh
-
-/Users/gangzhang/Documents/videotest1/app/src/main/jni/UsageEnvironment/include/HashTable.hh:
-
-/Users/gangzhang/Documents/videotest1/app/src/main/jni/UsageEnvironment/include/Boolean.hh:
--- app/src/main/obj/local/armeabi/objs/live555/liveMedia/MP3ADUdescriptor.o.d
+++
@@ -1,5 +0,0 @@
-/Users/gangzhang/Documents/videotest1/app/src/main/obj/local/armeabi/objs/live555/liveMedia/MP3ADUdescriptor.o: \
- /Users/gangzhang/Documents/videotest1/app/src/main/jni/liveMedia/MP3ADUdescriptor.cpp \
- /Users/gangzhang/Documents/videotest1/app/src/main/jni/liveMedia/MP3ADUdescriptor.hh
-
-/Users/gangzhang/Documents/videotest1/app/src/main/jni/liveMedia/MP3ADUdescriptor.hh:
